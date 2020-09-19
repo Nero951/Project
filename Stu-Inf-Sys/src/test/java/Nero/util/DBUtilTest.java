ca23def06e0b2c6441b0247b64190b8bd358861f
Nero951 <zhlwd103511@gmail.com>
2020-09-19 23:21:14 +0800

    Stu-Inf-Sys

--- Stu-Inf-Sys/src/test/java/Nero/util/DBUtilTest.java
+++ Stu-Inf-Sys/src/test/java/Nero/util/DBUtilTest.java
@@ -4,9 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DBUtilTest {
-
     @Test
     public void testConnection(){
+
         Assert.assertNotNull(DBUtil.getConnection());
     }
 }
